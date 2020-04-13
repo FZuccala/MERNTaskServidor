0a1e92cebddfb75aa06ae9bbdffd7f1cfc54e519
monazo22 <federico_zuccala@hotmail.com>
2020-04-13 09:44:41 -0300

    ajustes

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
+// Definir la pagina principal
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
 //arrancar la app
 app.listen(port, '0.0.0.0' ,  () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
